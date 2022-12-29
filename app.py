a28bd5344a01cad2103a5246076499830da3917c
Adeoye Victor <adeoyevictor16@gmail.com>
2022-12-29 03:04:26 +0100

    fixes

--- app.py
+++ app.py
@@ -25,15 +25,6 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 # Session(app)
 
-@app.route('/git_update', methods=['POST'])
-def git_update():
-    repo = git.Repo('./orbe')
-    origin = repo.remotes.origin
-    repo.create_head('main',
-                     origin.refs.main).set_tracking_branch(origin.refs.main).checkout()
-    origin.pull()
-    return '', 200
-
 @app.route('/')
 @login_required
 def index():
